9cb728cde57b6d87ee54998f82d37836110fb722
Christerle Nounoute <christerlesn@gmail.com>
2020-11-06 04:22:04 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,7 +7,9 @@ module Players
       move = nil
       if !board.taken?(5)
         move = "5"
-      elsif board.turn_count == 1 || board.turn_count == 2
+      elsif board.turn_count == 1
+        move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s 
+      elsif board.turn_count == 2
         move = [1,3,7,9].detect{|i| !board.taken?(i)}.to_s
       elsif board.turn_count == 3 &&
         board.position(1) == board.position(9) ||
